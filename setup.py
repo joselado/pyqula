8eafc54dac308eb516a02af8314b88e86455da5d
joselado <jose.luis.lado@gmail.com>
2021-07-28 20:28:11 +0300

    Update

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyqula-joselado",
+    version="0.0.3",
+    author="Jose Lado",
+    author_email="joselado@aalto.fi",
+    description="Python library for tight binding model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joselado/pyqula",
+    project_urls={
+        "Bug Tracker": "https://github.com/joselado/pyqula/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
