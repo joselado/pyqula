38f2a9799928fe6ed3b70c9660f914c7d17778f2
joselado <jose.luis.lado@gmail.com>
2021-08-19 13:04:46 +0300

    Update

--- src/pyqula/heterostructures.py
+++ src/pyqula/heterostructures.py
@@ -135,20 +135,9 @@ class heterostructure():
     fun_sl = intermatrix(fsl,xs=es) # get the function
     self.selfgen = [fun_sl,fun_sr] # store functions
     self.interpolated_selfenergy = True # set as true
-  def didv(self,energy=0.0,delta=None,error=1e-4,nk=10,kwant=False,
-          opl=None,opr=None):
-    if delta is None: delta = self.delta # set the own delta
-    if self.dimensionality==1: # one dimensional
-      return didv(self,energy=energy,delta=delta,kwant=kwant,
-              opl=opl,opr=opr) # return value
-    elif self.dimensionality==2: # two dimensional
-      # function to integrate
-      print("Computing",energy)
-      f = lambda k: self.generate(k,self.scale_lc,self.scale_rc).didv(energy=energy,delta=delta)
-      from .parallel import pcall
-      out = pcall(f,np.linspace(0.,1.,nk,endpoint=False))
-      return np.trapz(out,dx=1./nk)
-    else: raise
+  def didv(self,**kwargs):
+      from .transporttk.didv import generic_didv
+      return generic_didv(self,**kwargs)
   def block2full(self,sparse=False):
     """Put in full form"""
     return block2full(self,sparse=sparse)
---
+++ src/pyqula/transporttk/didv.py
@@ -0,0 +1,21 @@
+from ..parallel import pcall
+from ..heterostructures import didv
+import numpy as np
+
+# wrapper to compute the dIdV curve
+
+def generic_didv(self,energy=0.0,delta=None,error=1e-4,nk=10,kwant=False,
+          opl=None,opr=None):
+    if delta is None: delta = self.delta # set the own delta
+    if self.dimensionality==1: # one dimensional
+      return didv(self,energy=energy,delta=delta,kwant=kwant,
+              opl=opl,opr=opr) # return value
+    elif self.dimensionality==2: # two dimensional
+      # function to integrate
+      print("Computing",energy)
+      f = lambda k: self.generate(k,self.scale_lc,self.scale_rc).didv(energy=energy,delta=delta)
+      out = pcall(f,np.linspace(0.,1.,nk,endpoint=False))
+      return np.trapz(out,dx=1./nk)
+    else: raise
+
+
