7e59326e7142838bdca7659f28a466a3681f12c3
joselado <jose.luis.lado@gmail.com>
2021-08-21 11:39:12 +0300

    Update

--- examples/transport/decay_constant/main.py
+++ examples/transport/decay_constant/main.py
@@ -10,16 +10,16 @@ import matplotlib.pyplot as plt
 g = geometry.square_ribbon(1)
 #g = g.supercell(3)
 h = g.get_hamiltonian()
-h.shift_fermi(1.0) # shift the chemical potential
+h.shift_fermi(.5) # shift the chemical potential
 h1 = h.copy() # copy
 h2 = h.copy() # copy
 h1.add_swave(.0) # add electron hole symmetry
-#h2.add_swave(.1) # pairing gap of 0.01
-h2.add_pairing(mode="triplet",delta=0.05) # pairing gap of 0.01
+h2.add_swave(.1) # pairing gap of 0.01
+#h2.add_pairing(mode="triplet",delta=0.05) # pairing gap of 0.01
 ht = heterostructures.build(h1,h2) # create the junction
 ht.delta = 1e-12 # analytic continuation of the Green's functions
-es = np.linspace(-.2,.2,200) # grid of energies
-T = 1e-1 # reference transparency 
+es = np.linspace(-.2,.2,40) # grid of energies
+T = 3e-1 # reference transparency 
 ht.scale_rc = T # set the transparency for dIdV
 ts = [ht.didv(energy=e) for e in es] # calculate transmission
 ks = [ht.get_kappa(energy=e,T=T) for e in es] # compute kappa
