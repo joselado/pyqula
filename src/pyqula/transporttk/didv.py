f68ae3030eb418aabb15a1a51809bbcf9714f3fc
joselado <jose.luis.lado@gmail.com>
2021-08-22 10:46:00 +0300

    Update

--- src/pyqula/transporttk/didv.py
+++ src/pyqula/transporttk/didv.py
@@ -50,23 +50,42 @@ def finite_T_didv(self,temp,energy=0.0,**kwargs):
 #    return integ/norm
 
 
-def zero_T_didv(self,energy=0.0,delta=None,error=1e-4,nk=10,kwant=False,
-          opl=None,opr=None,**kwargs):
+def zero_T_didv(self,delta=None,**kwargs):
+    """Zero temperature dIdV"""
     if delta is None: delta = self.delta # set the own delta
     if self.dimensionality==1: # one dimensional
-        try:
-            return didv(self,energy=energy,delta=delta,kwant=kwant,
-              opl=opl,opr=opr) # return value
-        except: 
-            print("Something wrong in didv, returning 0")
-            return 1e-10
+        return zero_T_didv_1D(self,**kwargs)
     elif self.dimensionality==2: # two dimensional
-      # function to integrate
-      print("Computing",energy)
-      f = lambda k: self.generate(k,self.scale_lc,self.scale_rc).didv(energy=energy,delta=delta)
-      out = pcall(f,np.linspace(0.,1.,nk,endpoint=False))
-#      return simpson(f,eps=np.mean(out))
-      return np.trapz(out,dx=1./nk)
+        return zero_T_didv_2D(self,**kwargs)
     else: raise
+
+
+
+
+
+def zero_T_didv_1D(self,energy=0.0,delta=None,**kwargs):
+    """Wrapper for the dIdV in one dimension"""
+    if delta is None: delta = self.delta # set the own delta
+    if not self.dimensionality==1: raise # only for one dimensional
+    try:
+        return didv(self,energy=energy,delta=delta,**kwargs)
+    except:
+        print("Something wrong in didv, returning 0")
+        return 1e-10
+
+
+def zero_T_didv_2D(self,energy=0.0,delta=None,nk=10,**kwargs):
+    """Wrapper for the dIdV in two-dimensions"""
+    if delta is None: delta = self.delta # set the own delta
+    if not self.dimensionality==2: raise # only for two dimensional
+    # function to integrate
+    print("Computing",energy)
+    f = lambda k: self.generate(k,self.scale_lc,self.scale_rc).didv(energy=energy,delta=delta,**kwargs)
+    out = pcall(f,np.linspace(0.,1.,nk,endpoint=False))
+#    return simpson(f,eps=np.mean(out))
+    return np.trapz(out,dx=1./nk)
+
+
+
 
 
