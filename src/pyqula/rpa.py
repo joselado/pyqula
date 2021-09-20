3cac6beb379734170e954228e6192acb2d326109
joselado <jose.luis.lado@gmail.com>
2021-09-20 19:29:19 +0300

    Update

--- src/pyqula/rpa.py
+++ src/pyqula/rpa.py
@@ -4,6 +4,7 @@ from . import algebra
 # compute RPA response
 def chi_AB_RPA(h,V,**kwargs):
     """Compute the RPA chi for an SCF object"""
+    raise # not finished
     from .chi import chiAB # get response function
     chis = chiAB(h,**kwargs) # get the non-interacting response functions
     iden = np.identity(chis[0].shape[0],dtype=np.complex) # identity
