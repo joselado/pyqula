ffa1d4222534c6e4e230b1298f26d435730d81f3
joselado <jose.luis.lado@gmail.com>
2021-10-01 15:56:19 +0300

    Update

---
+++ examples/minimal/berry_curvature_sublattice_imbalance/main.py
@@ -0,0 +1,40 @@
+# Add the root path of the pyqula library
+import os ; import sys 
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
+
+
+
+
+
+# zigzag ribbon
+import numpy as np
+from pyqula import geometry
+g = geometry.honeycomb_lattice()
+h = g.get_hamiltonian() # create hamiltonian of the system
+h.add_sublattice_imbalance(.4) # add Haldane hopping to the Hamiltonian
+(k,e) = h.get_bands() # get the bandstructure (also written to BANDS.OUT)
+(kb,b) = h.get_berry_curvature() # get the Berry curvature
+
+import matplotlib.pyplot as plt
+
+# plot the bands
+plt.subplot(211)
+plt.scatter(k,e)
+plt.ylabel("Energy")
+plt.xlabel("k-path")
+plt.xticks([])
+# plot the Berry curvature
+plt.subplot(212)
+plt.plot(kb,b)
+plt.ylabel("Berry curvature")
+plt.xlabel("k-path")
+plt.xticks([])
+
+
+plt.show()
+
+
+
+
+
+
