1a2c7c6bed49bc0721bf00bd2f20f50009c7a8c9
joselado <jose.luis.lado@gmail.com>
2021-10-16 11:43:49 +0300

    Update

---
+++ examples/0d/non_uniform_strain/main.py
@@ -0,0 +1,20 @@
+# Add the root path of the pyqula library
+import os ; import sys 
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
+
+
+from pyqula import islands
+import numpy as np
+g = islands.get_geometry(name="honeycomb",n=3,nedges=3,rot=0.0) # get an island
+h = g.get_hamiltonian(has_spin=False) # get the Hamiltonian
+from pyqula import potentials
+fs = potentials.radial_decay(v0=2.0,voo=1.0,rl=3.0)
+h.add_strain(fs)
+h.geometry.write_profile(fs)
+h.write_hopping()
+h.get_bands()
+
+
+
+
+
--- src/pyqula/strain.py
+++ src/pyqula/strain.py
@@ -1,10 +1,32 @@
-import numpy as np
+
+# routines to apply strain to Hamiltonian
+
+def add_strain(h,f):
+    """Take as input a Hamiltonian, return the Hamiltonian
+    with applied strain"""
+    h.turn_multicell() # multicell Hamiltonian
+    if not h.has_eh:
+      if not h.has_spin: indg = lambda i: [i]
+      elif h.has_spin: indg = lambda i: [2*i,2*i+1]
+      else: raise
+    else: raise
+    def fm(m,r1,r2): # function to modify hopping
+        return strain_matrix(m,r1,r2,indg,f)
+    h.modify_hamiltonian_matrices(fm,use_geometry=True) # modify matrices
 
 
-def add_strain(h):
-    """Given a certain Hamiltonian, add strain to the
-    spin-independent hoppings"""
-    raise
 
 
+
+
+def strain_matrix(m,rs1,rs2,indg,fs): # function to apply strain to a matrix
+    mo = m.copy() # copy matrix
+    for i in range(len(rs1)): # loop over sites
+        for j in range(len(rs2)): # loop over sites
+            r0 = (rs1[i]+rs2[j])/2.
+            fac = fs(r0)
+            for ii in indg(i): # generate indexes for site i
+                for jj in indg(j): # generate indexes for site j
+                    mo[ii,jj] = fac*m[ii,jj] # strain the matrix
+    return mo
 
