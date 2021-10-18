95fb4ff0a32d5ad3b8c347e4cf0dd9e3f2ec892a
joselado <jose.luis.lado@gmail.com>
2021-10-18 13:45:30 +0300

    Update

---
+++ examples/2d/hopping_renormalization_V1/main.py
@@ -0,0 +1,34 @@
+# Add the root path of the pyqula library
+import os ; import sys 
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
+
+
+
+
+
+# zigzag ribbon
+import numpy as np
+from pyqula import geometry
+from scipy.sparse import csc_matrix
+from pyqula import meanfield
+g = geometry.triangular_lattice()
+h = g.get_hamiltonian() # create hamiltonian of the system
+h.remove_spin() # spinless Hamiltonian
+hscf = h.get_mean_field_hamiltonian(filling=0.5,nk=10,
+                        V1 = 1.0, # first enighbor interaction
+                        V2 = 1.0,
+                        verbose=1,
+                        mix=0.9, # mixing
+                        mf = "random" # random initial guess
+                        )
+(k,e) = h.get_bands()
+(kscf,escf) = hscf.get_bands()
+
+import matplotlib.pyplot as plt
+
+plt.scatter(k,e,label="no SCF")
+plt.scatter(kscf,escf,label="SCF")
+plt.legend()
+plt.show()
+
+
