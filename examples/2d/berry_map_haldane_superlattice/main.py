730a1726b1ba89b2ceb2b1d0ca995461f8696a5b
joselado <jose.luis.lado@gmail.com>
2021-11-21 14:07:13 +0200

    Update

--- examples/2d/berry_map_haldane_superlattice/main.py
+++ examples/2d/berry_map_haldane_superlattice/main.py
@@ -8,20 +8,20 @@ import numpy as np
 from pyqula import geometry
 from pyqula import topology
 g = geometry.honeycomb_lattice()
-g = g.supercell(3)
+g = g.supercell(6)
 h = g.get_hamiltonian(has_spin=True)
 rmax = np.sqrt(g.a1.dot(g.a1))
 def fm(r): 
   if np.sqrt(r.dot(r))<rmax/3: return 0.3
   else: return 0.0
-h.add_haldane(0.03)
+h.add_haldane(fm)
 h.add_swave(0.02)
 from pyqula import parallel
 topology.berry_green_map(h,
         energy = 0.5,
         k=[0.,0.,0.0],# do it just at the Gamma point
         nrep=3, # how many supercells to print
-        integral=False, # perform the integral to the Fermi energy
+        integral=True, # perform the integral to the Fermi energy
         eps=1e-4, # error for the integral
         delta=1e-2 # analytic continuation
         )
