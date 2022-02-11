b9fb7fa5b346b5ff0bab33c53d6a16ea2f6f8b5b
joselado <jose.luis.lado@gmail.com>
2022-02-11 16:38:28 +0200

    Update

---
+++ src/pyqula/embeddingtk/ingap.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.optimize import minimize
+import warnings
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+
+
+# library to target ingap states
+
+
+def energy_ingap_state(EO,ne=11):
+    """Return the energy of an in-gap state given an embedding object"""
+    ev = EO.h0.get_gap(mode="valence")
+    ec = EO.h0.get_gap(mode="conduction")
+    es = np.linspace(-ev*0.95,ec*0.95,ne) # number of energies
+    delta = (ev+ec)/ne # delta
+    (es,ds) = EO.multidos(es=es,delta=delta) # return DOS
+    fd = interp1d(np.array(es),-np.array(ds),bounds_error=False,
+                         fill_value=(ds[0],ds[-1]),
+                        kind="quadratic")
+    res = minimize(fd,np.array([0.0]),bounds=np.array([[min(es),max(es)]]),
+                          method="Powell")
+    return res.x[0]
+
