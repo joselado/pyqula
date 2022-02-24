9d2ebcb94a3ea74a2a72e2f93a9b1ca408fa7e38
joselado <jose.luis.lado@gmail.com>
2022-02-24 18:51:29 +0200

    Update

--- examples/transport/localprobe/main.py
+++ examples/transport/localprobe/main.py
@@ -11,15 +11,19 @@ from pyqula.heterostructures import LocalProbe
 import numpy as np
 import matplotlib.pyplot as plt
 g = geometry.chain()
-g = geometry.single_square_lattice()
+#g = geometry.single_square_lattice()
 h = g.get_hamiltonian()
-h.add_onsite(1.0)
-h.add_swave(0.2)
+h.add_onsite(2.0)
+h.add_exchange([0.,0.,0.6])
+h.add_rashba(0.6)
+h.add_swave(0.1)
+#h.get_bands() ; exit()
 lp = LocalProbe(h) # create a local probe object
+lp.mode = "surface" # mode of the calculation
 lp.delta = 1e-5
-lp.delta_bulk = 1e-2
-es = np.linspace(-0.4,0.4,10)
-lp.T = 1e-2
+lp.delta_bulk = 1e-3 # this is the selfenery of the bulk
+es = np.linspace(-0.2,0.2,201)
+lp.T = 4e-1
 ts = [lp.didv(energy=e) for e in es]
 
 import matplotlib.pyplot as plt
