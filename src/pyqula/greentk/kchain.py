04940ab45091b827e5fce38a97e8df00852961a8
joselado <jose.luis.lado@gmail.com>
2022-02-25 17:14:10 +0200

    Update

--- src/pyqula/greentk/kchain.py
+++ src/pyqula/greentk/kchain.py
@@ -30,3 +30,27 @@ def get1dhamiltonian(hin,k=[0.0,0.,0.],reverse=False):
   else: return (ons,hop) # return 
 
 
+
+
+def green_kchain_evaluator(h,k=0.,delta=0.01,only_bulk=True,
+                    error=0.0001,hs=None,reverse=False):
+  """ Calculates the green function of a kdependent chain for a 2d system """
+  def gr(ons,hop,energy):
+    """ Calculates G by renormalization"""
+    gf,sf = green_renormalization(ons,hop,energy=energy,nite=None,
+                            error=error,info=False,delta=delta)
+    if hs is not None: # surface matrix provided
+      ez = (energy+1j*delta)*np.identity(h.intra.shape[0]) # energy
+      sigma = hop@sf@algebra.dagger(hop) # selfenergy
+      if callable(hs): ons2 = ons + hs(k)
+      else: ons2 = ons + hs
+      sf = algebra.inv(ez - ons2 - sigma) # return Dyson
+    # which green function to return
+    if only_bulk:  return gf
+    else:  return gf,sf
+  (ons,hop) = get1dhamiltonian(h,k,reverse=reverse) # get 1D Hamiltonian
+  def fun(energy): # evaluator
+    return gr(ons,hop,energy)  # return green function
+  return fun # return the function
+
+
