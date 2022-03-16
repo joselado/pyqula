e7647579b2db546d3c09a72daf4fe78e6bedd73e
joselado <jose.luis.lado@gmail.com>
2022-03-16 19:42:11 +0200

    Update

--- examples/readme_examples/scf_SC_triplet/main.py
+++ examples/readme_examples/scf_SC_triplet/main.py
@@ -14,6 +14,7 @@ h = h.get_mean_field_hamiltonian(V1=-1.0,filling=0.3,mf="random")
 # compute the non-unitarity of the spin-triplet superconducting d-vector
 d = h.get_dvector_non_unitarity() # non-unitarity of spin-triplet
 # electron spectral-funtion
-h.get_kdos_bands(operator="electron",nk=400,energies=np.linspace(-2.0,2.0,400)) 
+h.get_kdos_bands(operator="electron",nk=400,energies=np.linspace(-2.0,2.0,400),
+                                               kpath=["G","K","M","G"]) 
 
 
