54d8bd62e5ac2c339159c6626b2f6773ce4bea2c
joselado <jose.luis.lado@gmail.com>
2022-03-23 10:02:10 +0200

    Update

--- examples/2d/multifermisurface/main.py
+++ examples/2d/multifermisurface/main.py
@@ -8,12 +8,11 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 import numpy as np
 from pyqula import geometry
-from pyqula import spectrum
-g = geometry.honeycomb_lattice()
-h = g.get_hamiltonian(has_spin=True)
-#h.get_qpi(delta=5e-2)
-spectrum.multi_fermi_surface(h,nk=60,energies=np.linspace(-4,4,100),
-        delta=0.1,nsuper=1)
+g = geometry.square_lattice()
+h = g.get_hamiltonian()
+h.get_qpi(delta=0.1,nk=80,energies=np.linspace(-4,4,101))
+
+
 
 
 
--- examples/2d/spiral_energy/main.py
+++ examples/2d/spiral_energy/main.py
@@ -7,48 +7,34 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 
 from pyqula import geometry
-from pyqula import scftypes
 import numpy as np
 # create the hamiltonian
 g = geometry.triangular_lattice() # triangular lattice geometry
-#g = geometry.chain()
-g = g.supercell(2)
-h0 = g.get_hamiltonian(has_spin=True) # create hamiltonian of the system
-###################
+h0 = g.get_hamiltonian() # create hamiltonian of the system
 
-# perform the SCF calculation
-#mf = scftypes.guess(h0,"ferro",fun=[1.,0.,0.]) # in-plane guess
-#scf = scftypes.selfconsistency(h0,filling=0.5,nkp=20,g=10.0,
-#           mf=mf,mix=0.8,maxerror=1e-6)
-#hscf = scf.hamiltonian # save the selfconsistent Hamiltonian
-#hscf.get_bands(operator="sz") # compute the SCF bandstructure
-#########################
-
-hscf = h0.copy() 
 
 # Now compute energies for different rotations
 # mesh of quvectors for the spin spiral
 qs = np.linspace(-1,1,20) # loop over qvectors
 from pyqula import klist
-qpath = klist.default(hscf.geometry,nk=100) # default qpath
+qpath = klist.default(h0.geometry,nk=100) # default qpath
 
 fo = open("STIFFNESS.OUT","w") # open file
 for iq in range(len(qpath)): # loop over qy
     q = qpath[iq] # q-vector
     # Compute the energy by rotating the previous ground state
     ############################
-    h = hscf.copy() # copy SCF Hamiltonian
+    h = h0.copy() # copy SCF Hamiltonian
     # create the qvector of the rotation
     # get the vector in natural units (assume input is in the real BZ)
     # This is the direction around which we rotate the magnetization
     vector = [0.,0.,1.]
     # rotate the Hamiltonian
-
-    h.generate_spin_spiral(vector=[0.,0.,1.],qspiral=q,
+    h.generate_spin_spiral(vector=vector,qspiral=q,
             fractional=True)
     h.add_zeeman([4.,0.,0.0])
     # compute the total energy by summing eigenvalues
-    e = h.total_energy(nk=5,mode="mesh") # energy computed by rotation
+    e = h.total_energy(nk=20,mode="mesh") # energy computed by rotation
     ############################
     # write in a file
     fo.write(str(iq)+"    "+str(e)+"\n") # write
