e6744990627cb9e7695e91c471af18171d139f41
joselado <jose.luis.lado@gmail.com>
2022-04-26 18:02:36 +0300

    Update

--- examples/2d/frustrated_lattice/main.py
+++ examples/2d/frustrated_lattice/main.py
@@ -10,14 +10,14 @@ from pyqula import geometry
 from pyqula import classicalspin
 g = geometry.honeycomb_lattice() # generate the geometry
 g = geometry.triangular_lattice() # generate the geometry
-g = g.supercell(8)
+g = g.supercell(9)
 sm = classicalspin.SpinModel(g) # generate a spin model
 sm.add_heisenberg() # add heisenber exchange
 classicalspin.use_jax = True
 sm.minimize_energy() # minimize Hamiltonian
 h = g.get_hamiltonian() # get the Hamiltonian
 h.add_magnetism(sm.magnetization*2.0) # add magnetization
-h.write_magnetization()
+h.write_magnetization(nrep=2)
 
 
 
--- examples/readme_examples/tbg/main.py
+++ examples/readme_examples/tbg/main.py
@@ -4,7 +4,10 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 
 from pyqula import specialhamiltonian # special Hamiltonians library
-h = specialhamiltonian.twisted_bilayer_graphene() # TBG Hamiltonian
-h.set_filling(.5) # put Fermi energy at half filling
-(k,e) = h.get_bands() # compute band structure
+h = specialhamiltonian.twisted_bilayer_graphene(13,ti=0.4) # TBG Hamiltonian
+h.set_filling(.5,nk=1) # put Fermi energy at half filling
+h.turn_sparse()
+from pyqula import parallel
+parallel.cores = 6
+(k,e) = h.get_bands(num_bands=20,nk=100) # compute band structure
 
