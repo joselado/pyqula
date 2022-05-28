c573416f3a5623d1f3d48b5952a960047743308c
joselado <jose.luis.lado@gmail.com>
2022-05-28 10:25:43 +0300

    Update

--- src/pyqula/checkclass.py
+++ src/pyqula/checkclass.py
@@ -1,17 +1,11 @@
 import collections
 import numpy as np
+from collections.abc import Iterable
 
 
 # function to check if inputs are of certain type
 
-def is_iterable(e): 
-    try: 
-       return isinstance(e, collections.Iterable)
-    except: # workaround
-       try: 
-           iter(e)
-           return True
-       except: return False
+def is_iterable(e): return isinstance(e,Iterable)
 
 def number2array(n,d=3):
     """Given a certain object, return an array"""
