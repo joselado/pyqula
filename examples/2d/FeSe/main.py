f12b403db98686dbc3a140c9dd9e8a890e48fbaf
joselado <jose.luis.lado@gmail.com>
2022-07-07 10:05:04 +0300

    Update

--- examples/2d/FeSe/main.py
+++ examples/2d/FeSe/main.py
@@ -5,10 +5,11 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 
 from pyqula import specialhamiltonian
+import numpy as np
 h = specialhamiltonian.FeSe()
-h.geometry.write()
+h.add_rashba(1.)
 h.get_bands()
-h.get_fermi_surface()
+h.get_multi_fermi_surface(nk=100,delta=1e-1,energies=np.linspace(-6,8.100))
 
 
 
