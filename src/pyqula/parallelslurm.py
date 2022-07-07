2bf1ae4e0ac16d632540ea3fdfc90378ff5659d4
joselado <jose.luis.lado@gmail.com>
2022-07-07 14:10:37 +0300

    Update

--- src/pyqula/parallelslurm.py
+++ src/pyqula/parallelslurm.py
@@ -35,14 +35,14 @@ def pcall_killproof_dict(fin,xs,info=True,**kwargs):
     print("Hello")
     outl = pcall_single(fin,xs,**kwargs) # the return is a list
     out = dict()
-    for i in range(len(outl)):
-        out[(xs[i],i)] = outl[i] # store
+    for i in range(len(ys)):
+        out[ys[i]] = outl[i] # store
     xsnew = [] # empty list
     for (x,o) in zip(xs,out): # loop over keys
         if out[o] is None: # this one has been killed/failed
             if info: 
                 print("Relaunching",o)
-            xsnew.append(o[0]) # store
+            xsnew.append(o) # store
     if len(xsnew)==0: 
         return out # all good
     else:
@@ -54,11 +54,9 @@ def pcall_killproof_dict(fin,xs,info=True,**kwargs):
 def pcall_killproof(fin,xs,return_mode="list",**kwargs):
     out = pcall_killproof_dict(fin,xs,**kwargs)
     if return_mode=="list": 
-        return [out[(xs[i],i)] for i in range(len(xs))]
+        return [out[y] for y in ys]
     elif return_mode=="dict": 
-        outd = dict()
-        for o in out: outd[o[0]] = out[o]
-        return outd
+        return out
 
 
 
