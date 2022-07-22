45822462243ce4a056d6826fb53d9fe10fce6776
joselado <jose.luis.lado@gmail.com>
2022-07-22 14:58:18 +0300

    Update

--- src/pyqula/parallelslurm.py
+++ src/pyqula/parallelslurm.py
@@ -23,8 +23,8 @@ def pcall(fin,xs,batch_size=1,**kwargs):
                 xsn.append(o) # store
                 o = [] # reset
         def fnew(y): return [fin(x) for x in y] # call this batch
-        #outs = pcall_single(fnew,xsn,**kwargs) # call the inputs
-        outs = pcall_killproof(fnew,xsn,**kwargs) # call the inputs
+        outs = pcall_single(fnew,xsn,**kwargs) # call the inputs
+        #outs = pcall_killproof(fnew,xsn,**kwargs) # call the inputs
         out = []
         for o in outs: out += o # add
         return out
