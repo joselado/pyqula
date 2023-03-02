20dd980b5a8f193cb677d3797d37ba20ab286b12
joselado <jose.luis.lado@gmail.com>
2023-03-02 18:06:05 +0200

    Update

--- examples/2d/twisted_hBN_structure/main.py
+++ examples/2d/twisted_hBN_structure/main.py
@@ -3,13 +3,15 @@ import os ; import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 from pyqula import specialgeometry
-g = specialgeometry.twisted_bilayer(5) # create the basic structure
+N = 8 # this is an integer controlling the size of the unit cell (twist angle)
+# the bigger the integer, the smaller the twist angle
+g = specialgeometry.twisted_bilayer(5) # create the basic twisted structure
 def sublattice2name(s):
     if s==1: return "B"
     else: return "N"
 g.atoms_names = [sublattice2name(s) for s in g.sublattice] # set species
 from pyqula.geometry import write_vasp
-write_vasp(g,s=1.45,namefile="twisted_hBN.vasp")
+write_vasp(g,s=1.45,namefile="twisted_hBN.vasp") # write structure in a file
 
 
 
