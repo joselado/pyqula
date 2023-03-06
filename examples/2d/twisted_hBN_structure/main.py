2c81ee28308433d99df54de07d49f76bdfebe524
joselado <jose.luis.lado@gmail.com>
2023-03-06 19:51:26 +0200

    Update

--- examples/2d/twisted_hBN_structure/main.py
+++ examples/2d/twisted_hBN_structure/main.py
@@ -3,13 +3,13 @@ import os ; import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 from pyqula import specialgeometry
-N = 4 # this is an integer controlling the size of the unit cell (twist angle)
+N = 5 # this is an integer controlling the size of the unit cell (twist angle)
 # the bigger the integer, the smaller the twist angle
 g = specialgeometry.twisted_bilayer(N) # create the basic twisted structure
 
 # if you want an orthorombic unit cell, uncomment these lines
-from pyqula.supercell import turn_orthorombic
-g = turn_orthorombic(g) # build an orthorombic one
+from pyqula.supercell import turn_orthorhombic
+g = turn_orthorhombic(g) # build an orthorhombic one
 
 def sublattice2name(s):
     if s==1: return "B"
