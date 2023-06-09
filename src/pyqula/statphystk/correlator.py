74b6e5e23f899de00bceefa07fa1a3f17f572b1a
joselado <jose.luis.lado@gmail.com>
2023-06-09 09:19:38 +0300

    Update

--- src/pyqula/statphystk/correlator.py
+++ src/pyqula/statphystk/correlator.py
@@ -1,27 +1,23 @@
 import numpy as np
 from numba import jit
 
-def get_nnc_v0(g,den,rmax=10.0,dr=1.0): 
-    """Compute the first N correlators"""
-    ds = np.linspace(1.,rmax,int(rmax/dr)) # output
-    r = g.r # locations
-    if g.dimensionality>0: # periodic boundaries
-        r = g.multireplicas(1) # replicas of the locations
-        den0 = den.copy() # copy
-        den = den0.copy() # start
-        for i in range(1,len(r)//len(ds0)): den = np.concatenate([den,den0])
-    cs = [get_nnci_jit(r,den,di,dr/2.) for di in ds]
-    return ds,cs
-
-
-
 def get_nnc(g,den,n=20,**kwargs): 
     """Compute the first N correlators"""
     g0 = g.copy() ; g0.dimensionality = 0 # zero dimensional
     ds = g0.neighbor_distances() # neighbor distances
     if len(ds)<n: n = len(ds)
     r = g0.r # locations
-    cs = [get_nnci_jit(r,den,di,1e-3) for di in ds[0:n]]
+    ri = r.copy() # copy locations
+    deni = den.copy()
+    if g.dimensionality>0: # periodic boundaries
+        rj = g.multireplicas(1) # replicas of the locations
+        den0 = den.copy() # copy
+        denj = den0.copy() # start
+        for i in range(1,len(rj)//len(den0)): denj = np.concatenate([denj,den0])
+    else:
+        rj = ri
+        denj = deni
+    cs = [get_nnci_jit(ri,rj,deni,denj,di,1e-3) for di in ds[0:n]]
     x,y = ds[0:len(cs)],np.array(cs) # distance and correlators
     return x,y
 
@@ -35,6 +31,27 @@ def get_nnc(g,den,n=20,**kwargs):
 
 
 
+@jit(nopython=True)
+def get_nnci_jit_v1(ri,rj,deni,denj,di,delta):
+    """Compute a single correlator"""
+    out = 0. # output value
+    no = 0. # counter
+    outi,outj,noi,noj = 0.,0.,0.,0.
+    for i1 in range(len(ri)):
+        for i2 in range(len(rj)):
+            r1 = ri[i1]
+            r2 = rj[i2]
+            dr = r1-r2
+            dr2 = np.sum(dr*dr)
+            if np.abs(dr2 - di**2)<delta:
+                out += deni[i1]*denj[i2]
+                no += 1.
+                outi += deni[i1]
+                outj += denj[i2]
+                noi += 1
+                noj += 1
+    if no==0: return 0.
+    return out/no  - outi/noi*outj/noj # return correlator
 
 
 
@@ -44,19 +61,20 @@ def get_nnc(g,den,n=20,**kwargs):
 
 
 @jit(nopython=True)
-def get_nnci_jit(r,den,di,delta):
+def get_nnci_jit(ri,rj,deni,denj,di,delta):
     """Compute a single correlator"""
     out = 0. # output value
     no = 0. # counter
-    den = den - np.mean(den) # redefine so that it has zero average
-    for i1 in range(len(r)):
-        for i2 in range(i1,len(r)):
-            r1 = r[i1]
-            r2 = r[i2]
+    deni = deni - np.mean(deni) # redefine so that it has zero average
+    denj = denj - np.mean(denj) # redefine so that it has zero average
+    for i1 in range(len(ri)):
+        for i2 in range(len(rj)):
+            r1 = ri[i1]
+            r2 = rj[i2]
             dr = r1-r2
             dr2 = np.sum(dr*dr)
             if np.abs(dr2 - di**2)<delta:
-                out += den[i1]*den[i2]
+                out += deni[i1]*denj[i2]
                 no += 1.
-    if no==0: return 0.
-    return out/no # - np.mean(den)*np.mean(den) # return correlator
+    if no==0: return 0. # no neighbor found
+    return out/no  #- np.mean(deni)*np.mean(denj) # return correlator
