15565bf77f123f55e7ea60e2b99420e10cc8ac02
joselado <jose.luis.lado@gmail.com>
2023-06-29 14:14:01 +0300

    Update

--- examples/2d/z2_kane_mele/main.py
+++ examples/2d/z2_kane_mele/main.py
@@ -10,7 +10,7 @@ from pyqula import geometry
 from pyqula import topology
 g = geometry.honeycomb_lattice()
 h = g.get_hamiltonian()
-h.add_kane_mele(0.022)
+h.add_kane_mele(0.05)
 parity = topology.z2_invariant(h)
 print("Z2 is ",parity)
 h.get_bands()
