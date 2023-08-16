765e48e89ef01948f4e05a5aec57b6f05fc6ca0e
joselado <jose.luis.lado@gmail.com>
2023-08-16 13:49:56 +0300

    Update

--- examples/2d/FeSe/main.py
+++ examples/2d/FeSe/main.py
@@ -3,14 +3,10 @@ import os ; import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 
-
 from pyqula import specialhamiltonian
 import numpy as np
-h = specialhamiltonian.FeSe()
-#h.add_rashba(1.)
-h.get_bands()
-h.get_multi_fermi_surface(nk=100,delta=1e-1,energies=np.linspace(-1,1.,100))
-
+h = specialhamiltonian.FeSe_GM() # get a toy model for FeSe
+h.get_fermi_surface(nk=100,delta=4e-1,e=0.) # get the Fermi surface
 
 
 
