758ee12369af4adb1fad8d23dac07b7075f1fb19
joselado <jose.luis.lado@gmail.com>
2023-09-28 15:39:11 +0300

    Update

--- examples/0d/plot_hoppings/main.py
+++ examples/0d/plot_hoppings/main.py
@@ -17,13 +17,6 @@ g.write()
 g.dimensionality = 0
 h = g.get_hamiltonian(has_spin=True)
 h = h.get_multicell()
-op = operators.get_valley_taux(h,projector=True)
-#h.clean()
-h.intra = op(0)
-#h.add_kekule(0.2)
-#h.add_haldane(0.3)
-#h1 = h.copy() ; h1.clean() ; h1.add_haldane(0.3) ; h.add_hamiltonian(h1)
-h.get_bands()
 from pyqula import groundstate
 groundstate.hopping(h,nrep=2) # write three replicas
 
