aa934fa344a021cc0699cfb7d6a91135a302a577
joselado <jose.lado@aalto.fi>
2024-01-08 16:47:05 +0200

    Update

--- src/pyqula/specialhamiltoniantk/highorderTI.py
+++ src/pyqula/specialhamiltoniantk/highorderTI.py
@@ -19,3 +19,21 @@ def square_2OTI(delta=0.):
     return h # return Hamiltonian
 
 
+
+def chain_1OTI(delta=0.):
+    """Return the Hamiltonian of a 2nd order TI in the square lattice"""
+    d = delta # redefine
+    g = geometry.chain()
+    g = g.get_supercell(2) # make a supercell
+    h = g.get_hamiltonian(has_spin=False)
+    m = h.intra*(1.-d) # decrease hopping
+    h = h*(1+d) # scale all
+    # this is should be written properly
+    h.intra = m
+    return h # return Hamiltonian
+
+
+
+
+
+
