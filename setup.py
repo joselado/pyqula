02b96da7b63e3868039570b3c78208ea32ee4c79
joselado <jose.lado@aalto.fi>
2024-02-02 13:54:42 +0200

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyqula",
-    version="0.0.65",
+    version="0.0.66",
     author="Jose Lado",
     author_email="joselado@aalto.fi",
     description="Python library for quantum lattice tight binding models",
