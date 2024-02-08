e0b87e769809f3e24020691c8945b53af5cf5e99
joselado <jose.lado@aalto.fi>
2024-02-08 09:33:45 +0200

    Update

--- examples/2d/wannier/main.py
+++ examples/2d/wannier/main.py
@@ -1,12 +1,17 @@
 import os ;  import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 from pyqula import wannier
+from pyqula import multicell
 
 
 # now read the Hamiltonian
 # you need two files, hamiltonian.wannier and wannier.win
 h = wannier.read_multicell_hamiltonian(input_file="hamiltonian.wannier")
-h.get_bands() # compute band structure
+#g = geometry.honeycomb_lattice()
+#h = g.get_hamiltonian()
+hf = multicell.bulk2ribbon(h,n=10)
+
+(k,e,op) = hf.get_bands(operator="xposition")
 
 
 
--- examples/2d/z2_transition/main.py
+++ examples/2d/z2_transition/main.py
@@ -13,17 +13,20 @@ import numpy as np
 g = geometry.honeycomb_lattice() # create geometry of a chain
 ms = np.linspace(0.,0.4,40)
 z2s,gaps = [],[]
+
+
+nn = 60
+
 for m in ms: # loop over masses
     # calculate the Z2 invariant for certain Zeeman and Rashba
     h = g.get_hamiltonian(has_spin=True) # get the Hamiltonian, spinfull
     h.add_kane_mele(0.05) # add SOC
     h.add_sublattice_imbalance(m) # add mass
-    z2 = topology.z2_invariant(h) # get the Z2
+    z2 = topology.z2_invariant(h,nk=nn,nt=nn) # get the Z2
     gap = h.get_gap()
     z2s.append(z2)
     gaps.append(gap)
 
-exit()
 
 import matplotlib.pyplot as plt
 
