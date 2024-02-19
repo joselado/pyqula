6427347b1be9cdd5a528107084c5ed864345275a
joselado <jose.lado@aalto.fi>
2024-02-19 14:18:54 +0200

    Update

--- examples/2d/find_symmetry/main.py
+++ examples/2d/find_symmetry/main.py
@@ -7,8 +7,9 @@ import numpy as np
 from pyqula import specialhamiltonian
 # Hamiltonian
 h = specialhamiltonian.multilayer_graphene(l=[0 for i in range(2)],ti=0.1)
-h.add_onsite(lambda r: r[2])
+#h.add_onsite(lambda r: r[2])
 from pyqula.symmetrytk.localsymmetry import all_permutations
 
 Us = all_permutations(h,only_permutation=True)
+print(Us)
 for U in Us: print(np.round(U,3))
