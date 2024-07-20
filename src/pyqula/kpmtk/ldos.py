5bb68408d443d85d7cf7444f0c7aae977a308425
joselado <jose.lado@aalto.fi>
2024-07-20 20:27:25 +0300

    Update

---
+++ src/pyqula/kpmtk/ldos.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+from .momenttoprofile import generate_profile
+
+# functions for calculation of local DOS
+
+def get_ldos(m_in,i=0,scale=10.,x=None,
+        npol=None,ne=500,kernel="jackson",**kwargs):
+  """Return two arrays with energies and local DOS"""
+  if npol is None: npol = ne
+  mus = moments_local_dos(m_in/scale,
+          i=i,n=npol,**kwargs) # get coefficients
+  xs = np.linspace(-1.0,1.0,ne,endpoint=True)*0.99 # energies
+  ys = generate_profile(mus,xs,kernel=kernel)
+  xs,ys = scale*xs,ys/scale # rescale data
+  if x is not None:
+    from scipy.interpolate import interp1d
+    f = interp1d(xs,ys,bounds_error=False,fill_value=(ys[0],ys[-1]))
+    return x,f(x)
+  else: return xs,ys
+
+
+
+
+from .kpmnumba import kpm_moments_v
+
+def moments_local_dos(m,i=0,n=200,**kwargs):
+  """ Calculates local DOS using the KPM"""
+  nd = m.shape[0] # length of the matrix
+  mus = np.array([0.0j for j in range(2*n)])
+  v = index2vector(i,nd) # generate vector
+# get the chebychev moments
+  return kpm_moments_v(v,m,n=n,**kwargs)
+
+
+
+
+
+def index2vector(i,size):
+    """Transform an index into a vector"""
+    v = np.zeros(size,dtype=np.complex_) # initialize
+    if is_iterable(i): # assume it is a collection of indexes 
+        for ii in i: v[i] = 1.0 # assign
+        v = v/np.sqrt(np.sum(np.abs(v)**2)) # normalize
+    else: # assume it is an integer
+        v[i] = 1.0
+    return v
+
+
+from collections.abc import Iterable
+def is_iterable(e): return isinstance(e,Iterable)
+
---
+++ src/pyqula/kpmtk/momenttoprofile.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+
+
+
+from .kernels import fejer_kernel
+from .kernels import lorentz_kernel
+from .kernels import jackson_kernel
+
+
+
+def generate_profile(mus,xs,kernel="jackson",**kwargs):
+  """ Uses the Chebychev expansion to create a certain profile"""
+  # initialize polynomials
+#  xs = np.array([0.])
+  tm = np.zeros(xs.shape) +1.
+  t = xs.copy()
+  if kernel=="jackson": mus = jackson_kernel(mus)
+  elif kernel=="lorentz": mus = lorentz_kernel(mus)
+  elif kernel=="fejer": mus = fejer_kernel(mus)
+  else: raise
+  ys = np.zeros(xs.shape,dtype=np.complex_) + mus[0] # first term
+  # loop over all contributions
+  for i in range(1,len(mus)):
+    mu = mus[i]
+    ys += 2.*mu*t # add contribution
+    tp = 2.*xs*t - tm # chebychev recursion relation
+    tm = t + 0.
+    t = 0. + tp # next iteration
+  ys = ys/np.sqrt(1.-xs*xs) # prefactor
+  ys = ys/np.pi
+  return ys
+
+
+
+
+def generate_green_profile(mus,xs,kernel="jackson",**kwargs):
+  """ Uses the Chebychev expansion to create a certain profile"""
+  # initialize polynomials
+  tm = np.zeros(xs.shape) +1.
+  t = xs.copy()
+  ys = np.zeros(xs.shape,dtype=np.complex_) + mus[0]/2 # first term
+  if kernel=="jackson": mus = jackson_kernel(mus)
+  elif kernel=="lorentz": mus = lorentz_kernel(mus)
+  elif kernel=="fejer": mus = fejer_kernel(mus)
+  else: raise
+  for i in range(1,len(mus)): # loop over mus
+    ys += np.exp(1j*i*np.arccos(xs))*mus[i] # add contribution
+  ys = ys/np.sqrt(1.-xs*xs)
+  return 1j*2*ys/np.pi
+
+
+
+
+
+
