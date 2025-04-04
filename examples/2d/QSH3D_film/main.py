9b9456ce5cc8ec5b81d8c9831a03069989fad0d4
joselado <jose.lado@aalto.fi>
2025-04-04 10:16:00 +0300

    Update

--- examples/2d/QSH3D_film/main.py
+++ examples/2d/QSH3D_film/main.py
@@ -8,17 +8,8 @@ from pyqula import geometry,films
 import numpy as np
 # generate a film of a 3D QSH
 W=20
-g0 = geometry.diamond_lattice_minimal()
-g = films.geometry_film(g0,nz=W)
-zmax = np.max(g.r[:,2])
-zmin = np.min(g.r[:,2])
-def rem(r):
-    if abs(r[2]-zmax)<0.01: return True
-    if abs(r[2]-zmin)<0.01: return True
-    return False
-g = g.remove(rem)
 
-h = specialhamiltonian.QSH3D_film(g=g,soc=0.1) 
+h = specialhamiltonian.QSH3D_film(W=W,soc=0.1) 
 (k,e,c) = h.get_bands(operator="surface")
 
 import matplotlib.pyplot as plt
