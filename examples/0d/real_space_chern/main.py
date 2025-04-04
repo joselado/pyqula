7a9bf6e0e15a4030d5636e5d672eb414c1015fb9
joselado <jose.lado@aalto.fi>
2025-04-04 10:16:00 +0300

    Update

--- examples/0d/real_space_chern/main.py
+++ examples/0d/real_space_chern/main.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 
 from pyqula import islands
 import numpy as np
-g = islands.get_geometry(name="honeycomb",n=10,nedges=4,rot=0.0,clean=False) 
+g = islands.get_geometry(name="honeycomb",n=14,nedges=4,rot=0.0,clean=False) 
 h = g.get_hamiltonian(has_spin=False)
 h.add_haldane(.1)
 from pyqula import topology
