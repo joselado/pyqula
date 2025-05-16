0407bac27f8532329bafbf4694cb0f81c4a9555f
joselado <jose.lado@aalto.fi>
2025-05-16 16:08:36 +0300

    Update

--- src/pyqula/specialhamiltoniantk/decorated.py
+++ src/pyqula/specialhamiltoniantk/decorated.py
@@ -2,12 +2,12 @@ import numpy as np
 from .. import geometry
 from .. import potentials
 
-def decorated_triangular(t1=1.0,t2=1.0):
+def decorated_triangular(t1=1.0,t2=1.0,**kwargs):
     """Return the Hamiltonian of a decorated honeycomb lattice,
     namely a triangular lattice with sqrt(3) dimerization"""
     g = geometry.triangular_lattice_tripartite()
     g.shift(-g.r[2])
-    h = g.get_hamiltonian(has_spin=False)
+    h = g.get_hamiltonian(has_spin=False,**kwargs)
     pot = potentials.commensurate_potential(g,n=6,minmax=[-.2,1.])
     pot = pot.redefine(lambda v: 1.*(v>0))
 #    pot = 1.-pot/2.
