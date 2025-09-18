a99d4030fca1fdd998b7f2bb85503aa7c4d6e07e
joselado <jose.lado@aalto.fi>
2025-09-19 01:12:53 +0300

    Update

--- src/pyqula/fermisurface.py
+++ src/pyqula/fermisurface.py
@@ -17,7 +17,7 @@ def fermi_surface_generator(h,
                     energies=[0.0],nk=50,nsuper=1,reciprocal=True,
                     delta=1e-2,refine_delta=1.0,operator=None,
                     full_bz=False, # special flag for unfolding
-                    numw=20,info=True):
+                    numw=20,info=False):
   """Calculates the Fermi surface of a 2d system"""
   if h.is_sparse: mode = "sparse"
   else: mode = "full"
@@ -42,6 +42,7 @@ def fermi_surface_generator(h,
       if operator is None:
           if mode=='full': es = algebra.eigvalsh(hk) # get eigenvalues
           elif mode=='sparse': es = algebra.smalleig(hk,numw=numw)
+          # maybe this should be vectorized
           ws = [np.sum(delta/((e-es)**2+delta**2)) for e in energies] # weights
           return np.array(ws) # return weights
       else:
@@ -74,22 +75,30 @@ def fermi_surface_generator(h,
   return energies,rs,kdos
 
 
-def multi_fermi_surface(h,nk=None,delta=1e-2,
+def multi_fermi_surface(h,nk=None,delta=1e-2,write=True,
         output_folder="MULTIFERMISURFACE",**kwargs):
     """Compute several fermi surfaces"""
     if nk is None: nk = int(10./delta)
     energies,rs,kdos = fermi_surface_generator(h,nk=nk,delta=delta,**kwargs)
-    fs.rmdir(output_folder) # remove folder
-    fs.mkdir(output_folder) # create folder
-    fo = open(output_folder+"/"+output_folder+".TXT","w")
+    if write: 
+        fs.rmdir(output_folder) # remove folder
+        fs.mkdir(output_folder) # create folder
+        fo = open(output_folder+"/"+output_folder+".TXT","w")
+    nki = rs.shape[0]
+    out = np.zeros((len(energies),3,nki)) # create storage for the output
     for i in range(len(energies)): # loop over energies
         filename = output_folder+"_"+str(energies[i])+"_.OUT" # name
         name = output_folder+"/"+filename
-        np.savetxt(name,np.array([rs[:,0],rs[:,1],kdos[:,i]]).T)
-        fo.write(filename+"\n")
-    name = output_folder+"/DOS.OUT"
-    name = "DOS.OUT"
-    np.savetxt(name,np.array([energies,np.sum(kdos,axis=0)]).T)
-    fo.close()
+        o = np.array([rs[:,0],rs[:,1],kdos[:,i]]) # output
+        out[i,:,:] = o # store
+        if write:  
+            np.savetxt(name,o.T) # write in file
+            fo.write(filename+"\n")
+    if write: # if output is written
+        name = output_folder+"/DOS.OUT"
+        name = "DOS.OUT"
+        np.savetxt(name,np.array([energies,np.sum(kdos,axis=0)]).T)
+        fo.close()
+    return out # return all fermi surfaces
 
 
