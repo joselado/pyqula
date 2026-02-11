5e212d173945553f94a264b6c997dc9241c64c9d
joselado <jose.lado@aalto.fi>
2026-02-11 12:54:44 +0200

    Update

--- src/pyqula/parallelslurm.py
+++ src/pyqula/parallelslurm.py
@@ -105,6 +105,7 @@ def pcall_single(fin,xs,time=10,memory=5000,error=None,
     mins = str(max([mins,1])) # at least 1 minute
     runsh = "#!/bin/bash\n#SBATCH -n 1\n#SBATCH -t "+str(int(time))+":"+str(mins)+":00\n"
     runsh += "#SBATCH --mem-per-cpu="+str(memory)+"\n"
+    runsh += "#SBATCH --exclude=milan\n"
     runsh += "#SBATCH --array=0-"+str(n-1)+"\n"
     if constraint is not None:
         runsh += "#SBATCH --constraint="+str(constraint)+"\n"
