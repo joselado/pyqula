75750079160bc3c69b9ab5221a00f68b52af0150
joselado <jose.lado@aalto.fi>
2026-02-18 20:30:00 +0200

    Update

---
+++ examples/2d/square_altermagnet/main.py
@@ -0,0 +1,24 @@
+# Add the root path of the pyqula library
+import os ; import sys 
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
+
+
+
+
+
+import numpy as np
+from pyqula import specialhamiltonian
+
+h = specialhamiltonian.square_altermagnet(am=0.4)
+ks = [[np.cos(k),np.sin(k),0.] for k in np.linspace(0.,1.,100)*np.pi*2]
+
+(k,e,c) = h.get_bands(kpath=ks,operator="sz")
+
+h.get_fermi_surface(operator="sz",e=3.0,nk=80)
+
+import matplotlib.pyplot as plt
+
+plt.scatter(k,e,c=c,cmap="bwr")
+
+plt.show()
+
