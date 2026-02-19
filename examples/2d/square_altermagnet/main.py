6e279739c5b620fd4ceef4cf0447cb01c1495d0b
joselado <jose.lado@aalto.fi>
2026-02-19 08:06:20 +0200

    Update

--- examples/2d/square_altermagnet/main.py
+++ examples/2d/square_altermagnet/main.py
@@ -9,12 +9,12 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../../../src")
 import numpy as np
 from pyqula import specialhamiltonian
 
-h = specialhamiltonian.square_altermagnet(am=0.4)
+h = specialhamiltonian.square_altermagnet(am=.3)
 ks = [[np.cos(k),np.sin(k),0.] for k in np.linspace(0.,1.,100)*np.pi*2]
 
 (k,e,c) = h.get_bands(kpath=ks,operator="sz")
 
-h.get_fermi_surface(operator="sz",e=3.0,nk=80)
+h.get_fermi_surface(operator="sz",e=1.0,nk=80)
 
 import matplotlib.pyplot as plt
 
