791ac3e5e45ae97d508061b196f2af260c19cc75
joselado <jose.lado@aalto.fi>
2026-05-25 10:04:06 +0300

    Update

--- examples/2d/strained_honeycomb/main.py
+++ examples/2d/strained_honeycomb/main.py
@@ -23,10 +23,32 @@ from pyqula.strain import graphene_buckling
 pot = graphene_buckling(omega=omega,dt=0.2,geometry=g) # special function for buckled lattices
 h.add_strain(pot,mode="non_uniform")
 h.write_hopping()
-h.get_bands(num_bands=20)
-h.get_ldos(e=0.)
+(kb,eb) = h.get_bands(num_bands=20)
+(x,y,ld) = h.get_ldos(e=0.,nrep=2)
+h.turn_dense()
+(e,d) = h.get_dos(energies=np.linspace(-3.5,3.5,500),nk=30,delta=1e-2)
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(12,4))
+
+plt.subplot(1,3,1)
+plt.scatter(kb,eb,c="black",s=10)
+plt.ylabel("Energy") ; plt.xlabel("Momentum") ; plt.xticks([])
 
 
+plt.subplot(1,3,2)
+plt.scatter(x,y,c=ld,cmap="inferno")
+plt.xlabel("x") ; plt.ylabel("y") ; plt.axis("equal")
+
+
+plt.subplot(1,3,3)
+plt.plot(e,d)
+plt.xlabel("Energy") ; plt.ylabel("DOS") ; plt.ylim([0,max(d)]) 
+
+plt.tight_layout()
+
+plt.show()
 
 
 
