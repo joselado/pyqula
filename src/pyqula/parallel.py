edab7d0477659ef61dbf42b75a2939c463d7d278
joselado <jose.lado@aalto.fi>
2026-07-15 00:15:24 +0300

    Update

--- src/pyqula/parallel.py
+++ src/pyqula/parallel.py
@@ -13,6 +13,19 @@ def set_num_threads():
     else:
         numba.set_num_threads(1) # single thread
 
-from .paralleltk.multiprocess import set_cores
-from .paralleltk.multiprocess import pcall
-from .paralleltk.multiprocess import _num_cores as cores
+#from .paralleltk.multiprocess import set_cores
+#from .paralleltk.multiprocess import pcall
+#from .paralleltk.multiprocess import _num_cores as cores
+#from .paralleltk.jlib import pcall
+#from .paralleltk.jlib import set_cores
+#from .paralleltk.jlib import _cores as cores
+
+# parallelization not working yet, workaround
+cores = 1
+def pcall(f,xs,**kwargs):  return [f(x) for x in xs]
+def set_cores(n):  
+    global cores
+    cores=1
+
+
+
